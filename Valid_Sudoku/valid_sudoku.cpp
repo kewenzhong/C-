d948313fa2c351eaea06900b25886989000872a8
kewenzhong <2515727000@qq.com>
2019-03-03 19:01:18 +0800

    the Susoku program that can process crectly

--- Valid_Sudoku/valid_sudoku.cpp
+++ Valid_Sudoku/valid_sudoku.cpp
@@ -3,75 +3,139 @@
 using namespace std ;
 
 class Solution {
+private :
+    //record the space cells,and the possible value of the sapce cells 
+    // vector<vector<vector<char>>> valid ;
+    
 public:
-    bool isValidSudoku(vector<vector<char> >& board) {
-        char a[9];
-
+    bool solveSudoku(vector<vector<char>>& board) {
+        
         for( int i=0;i<9;i++ ){
-        	// a[0][i] = 'a'+i ;
-        	a[i] = '0' ; 
+            for( int j=0;j<9;j++ ){
+                
+                if( board[i][j] == '.' ){
+                    vector<char> av = availableValue(i,j,board) ;
+                    
+                    if( av.empty() ){
+                        // cout<<"empty"<<endl ;
+                        return false;
+                    }
+                    
+                    int size = av.size() ;
+                
+                    for( int k=0;k<size;k++ ){
+                        board[i][j] = av[k] ;
+                        
+                        // printRes(board) ;
+                        
+                        bool res = solveSudoku(board) ;
+                        // cout<<res<<endl ;
+                        if( res ){
+                            return true ;
+                        }
+                        else{
+                            board[i][j] = '.' ;
+                        }
+                    }
+                    
+                    if( board[i][j] == '.' ){
+                        return false ;
+                    }
+                    
+                   
+                }
+            }
         }
-
-        //row
-        for( int i=0;i<9;i++ ){
-        	for( int j=0;j<9;j++ ){
-        		if( board[i][j] != '.' ){
-        			int num = board[i][j] - '0' ;
-        			if( a[num] != '0' ){
-        				return false ;
-        			}
-        			else{
-        				a[num] += 1 ;
-        			}
-        		}
-        	}
-
-        	reset(a) ;
-        }
-
-        //column
-        for( int i=0;i<9;i++ ){
-        	for( int j=0;j<9;j++ ){
-        		if( board[j][i] != '.' ){
-        			int num = board[j][i] - '0' ;
-        			if( a[num] != '0' ){
-        				return false ;
-        			}
-        			else{
-        				a[num] += 1 ;
-        			}
-        		}
-        	}
-
-        	reset(a) ;
-        }
-
-        //each 3x3
-        for( int k=0;k<9;k++ ){
-        	for( int i=3*(k/3);i<3*(k/3)+3;i++ ){
-        		for( int j=3*(k%3);j<3*(k%3)+3;j++ ){
-        			if( board[j][i] != '.' ){
-        				int num = board[j][i] - '0' ;
-        				if( a[num] != '0' ){
-        					return false ;
-        				}
-        				else{
-        					a[num] += 1 ;
-        				}
-        			}
-
-        		}
-        	}
-        }
-
+        // printRes(board) ;
         return true ;
     }
-
-    void reset(char *a){
-    	for( int i=0;i<9;i++ ){
-    		a[i] = '0' ;
-    	}
+    
+    //find the available value of the specific cell
+    vector<char> availableValue(const int x,const int y,const vector<vector<char>>& board){
+        int a[10] = {0} ;
+        
+        for( int i=0;i<9;i++ ){
+            if( board[x][i] != '.' ){
+                a[board[x][i]-'0'] = 1 ;
+            }
+        }
+        
+        for( int i=0;i<9;i++ ){
+            if( board[i][y] !='.' ){
+                a[board[i][y]-'0'] = 1 ;
+            }
+        }
+        
+        int x0 = 3*(x/3) ;
+        int y0 = 3*(y/3) ;
+        
+        for( int i=x0;i<x0+3;i++ ){
+            for( int j=y0;j<y0+3;j++ ){
+                if( board[i][j] != '.' ){
+                     a[board[i][j]-'0'] = 1 ;
+                }
+            }
+        }
+        
+        vector<char> v ;
+        for( int i=1;i<10;i++ ){
+            // cout<<a[i]<<" ";
+            if( a[i]==0 ){
+                v.push_back('0'+i) ;
+            }
+        }
+        // cout<<endl ;
+        
+        // cout<<"x="<<x+1<<"  y="<<y+1<<endl ;
+        // for ( auto i:v ){
+        //     cout<<i<<" " ;
+        // }
+        // cout<<endl ;
+        
+        return v ;
+        
     }
+    
+    void printRes(const vector<vector<char>>& board){
+        for (int i=0;i<9;i++ ){
+            for( int j=0;j<9;j++ ){
+                cout<<setw(6)<<left<<board[i][j] ;
+            }
+            cout<<endl ;
+        }
+        cout<<endl ;
+        cout<<endl ;
+    }
+    
+//     //
+//     bool isIllegal(int x,int y,char c,vector<vector<char>>& board){
+        
+//     }
+    
+//     /*
+//     *   initiate the 3D vector,set its height 10 ,width 10
+//     *   mark the cells that is not space
+//     *   where space cell is marked '1',otherwise '0'
+//     */
+//     void initValid(vector<vector<vector<char>>>& valid,const vector<vector<char>>& board){
+//         valid.resize(10) ;
+//         for( int i=0;i<10;i++ ){
+//             valid[i].resize(10) ;
+//         }
+        
+//         for( int i=0;i<9;i++ ){
+//             for( int j=0;j<9;j++ ){
+//                 if( board[i][j] != '.' ){
+//                     valid[i][j][0] = '1' ;
+//                 }
+//                 else{
+//                     valid[i][j][0] = '0' ;
+//                 }
+//             }
+//         }
+        
+//     }
+        
 };
 
 int main(){
